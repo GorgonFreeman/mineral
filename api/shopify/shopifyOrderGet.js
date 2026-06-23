cd9f3c34875a48944643fb6b679a60b8c50e894a
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-06-24 02:29:29 +1000

    compose migrate to customFetch

--- api/shopify/shopifyOrderGet.js
+++ api/shopify/shopifyOrderGet.js
@@ -1,4 +1,4 @@
-const { credsFromPayload } = require('../utils');
+const { credsFromPayload, customFetch } = require('../utils');
 const { credsValidator } = require('../validators');
 
 const shopifyOrderGet = async (
@@ -25,24 +25,25 @@ const shopifyOrderGet = async (
 
   const orderGid = `gid://shopify/Order/${ orderId }`;
 
-  const response = await fetch(
+  const fetchResponse = await customFetch(
     `https://${ STORE_HANDLE }.myshopify.com/admin/api/2024-10/graphql.json`,
     {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
         'X-Shopify-Access-Token': API_KEY,
       },
-      body: JSON.stringify({
+      body: {
         query: `query { order(id: "${ orderGid }") { id name createdAt displayFinancialStatus displayFulfillmentStatus } }`,
-      }),
+      },
     },
   );
 
-  const responseData = await response.json();
-  console.log('responseData', responseData);
+  if (!fetchResponse.ok) {
+    return fetchResponse;
+  }
 
-  return responseData;
+  console.log({ fetchResponse });
+  return fetchResponse;
 };
 
 module.exports = {
