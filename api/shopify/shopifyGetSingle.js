986ad138a44c7955444c24337dafd3774337bc9e
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-06-25 09:26:48 +1000

    compose shopifyGetSingle

---
+++ api/shopify/shopifyGetSingle.js
@@ -0,0 +1,87 @@
+const { credsFromPayload } = require('../utils');
+const { credsValidator } = require('../validators');
+const { shopifyClient } = require('./shopify.utils');
+
+const defaultAttrs = 'id';
+
+const capitaliseString = (value) => `${ value[0].toUpperCase() }${ value.slice(1) }`;
+
+const gidFromId = (id, resource, { gidType } = {}) => {
+  if (id?.startsWith('gid://')) {
+    return id;
+  }
+
+  const resourceType = gidType || capitaliseString(resource);
+  return `gid://shopify/${ resourceType }/${ id }`;
+};
+
+const shopifyGetSingle = async (
+  credsPayload,
+  resource,
+  id,
+  options = {},
+) => {
+  const {
+    apiVersion = '2024-10',
+    attrs = defaultAttrs,
+    gidType,
+  } = options;
+  const creds = credsFromPayload(credsPayload);
+
+  const Resource = capitaliseString(resource);
+  const isShopResource = resource === 'shop';
+
+  const query = `
+    query Get${ Resource }${ isShopResource ? '' : '($id: ID!)' } {
+      ${ resource }${ isShopResource ? '' : '(id: $id)' } {
+        ${ attrs }
+      }
+    }
+  `;
+
+  const response = await shopifyClient.fetch({
+    method: 'post',
+    body: {
+      query,
+      ...isShopResource
+        ? {}
+        : {
+          variables: {
+            id: gidFromId(id, resource, { gidType }),
+          },
+        },
+    },
+    context: {
+      creds,
+      apiVersion,
+      resultPath: `data.${ resource }`,
+    },
+  });
+
+  return response;
+};
+
+const funcApiConfig = {
+  requestHandler: async ({ body }) => {
+    const normalisedBody = {
+      ...body,
+      options: body?.options || {},
+    };
+
+    return {
+      body: normalisedBody,
+    };
+  },
+  argNames: ['credsPayload', 'resource', 'id', 'options'],
+  validatorsByArg: {
+    credsPayload: (credsPayload) => credsValidator(credsPayload),
+    resource: (resource) => Boolean(resource),
+    id: (id, body) => body?.resource === 'shop' || Boolean(id),
+    options: () => true,
+  },
+};
+
+module.exports = {
+  shopifyGetSingle,
+  funcApiConfig,
+};
