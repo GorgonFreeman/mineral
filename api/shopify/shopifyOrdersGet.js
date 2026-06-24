685bea265d06896734f53fd10400ac6064ff61af
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-06-24 16:56:48 +1000

    compose shopifyOrdersGet

---
+++ api/shopify/shopifyOrdersGet.js
@@ -0,0 +1,74 @@
+const { credsFromPayload, logDeep } = require('../utils');
+const { credsValidator } = require('../validators');
+const { shopifyClient } = require('./shopify.utils');
+
+const shopifyOrdersGet = async (
+  credsPayload,
+  {
+    first = 50,
+    after,
+    query,
+  } = {},
+) => {
+
+  if (!credsValidator(credsPayload)) {
+    return {
+      ok: false,
+      error: {
+        code: 'INVALID_ARGS',
+        message: 'Invalid creds',
+      },
+    };
+  }
+
+  const creds = credsFromPayload(credsPayload);
+
+  const response = await shopifyClient.fetch({
+    method: 'post',
+    body: {
+      query: `
+        query OrdersGet(
+          $first: Int!,
+          $after: String,
+          $query: String
+        ) {
+          orders(
+            first: $first,
+            after: $after,
+            query: $query
+          ) {
+            edges {
+              node {
+                id
+                name
+                createdAt
+                displayFinancialStatus
+                displayFulfillmentStatus
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        first,
+        after,
+        query,
+      },
+    },
+    context: {
+      creds,
+      resultPath: 'data.orders',
+    },
+  });
+
+  if (!response.ok) {
+    return response;
+  }
+
+  logDeep('response', response);
+  return response;
+};
+
+module.exports = {
+  shopifyOrdersGet,
+};
