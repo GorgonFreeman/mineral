4343044371578e9b150dd8827d3115e500dfcfb9
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-06-24 10:14:38 +1000

    attempt to walk a result path

--- api/shopify/shopify.utils.js
+++ api/shopify/shopify.utils.js
@@ -10,12 +10,43 @@ const shopifyResponseHandler = (response, { resultPath }) => {
     return response;
   }
 
+  if (!resultPath || !response?.data) {
+    return response;
+  }
+  
+  // TODO: Handle array vs dot paths, and trace full path
+
+  let desiredData = response;
+  let omittedData;
+  let lastKey;
+
+  for (const key of ['data', resultPath]) {
+    const { [key]: value, ...rest } = desiredData;
+
+    if (!value) {
+      throw new Error(`resultPath didn't work`);
+    }
+
+    desiredData = value;
+
+    if (omittedData) {
+      omittedData[lastKey] = rest;
+    } else {
+      omittedData = { ...rest };
+    }
+
+    lastKey = key;
+  }
+
   return {
     ...response,
-    ...response?.data ? {
-      // TODO: Handle array vs dot paths, and trace full path
-      data: response?.data?.[resultPath],
-    } : {},
+    ...{
+      data: desiredData,
+      meta: {
+        ...response?.meta,
+        ...omittedData,
+      },
+    },
   };
 };
 
