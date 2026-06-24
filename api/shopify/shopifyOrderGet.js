bb96bd6bf71588a362829a9a09bfe5b026c114db
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-06-24 15:36:02 +1000

    pre-emptively update shopifyOrderGet to desired format

--- api/shopify/shopifyOrderGet.js
+++ api/shopify/shopifyOrderGet.js
@@ -1,6 +1,6 @@
-const { credsFromPayload, customFetch, logDeep, FetchClient } = require('../utils');
+const { credsFromPayload, logDeep } = require('../utils');
 const { credsValidator } = require('../validators');
-const { shopifyResponseHandler } = require('./shopify.utils');
+const { shopifyFetchClient } = require('./shopify.utils');
 
 const shopifyOrderGet = async (
   credsPayload,
@@ -19,26 +19,16 @@ const shopifyOrderGet = async (
 
   const creds = credsFromPayload(credsPayload);
 
-  const {
-    STORE_HANDLE,
-    API_KEY,
-  } = creds;
-
   const orderGid = `gid://shopify/Order/${ orderId }`;
 
-  const shopifyFetchClient = new FetchClient({
-    url: `https://${ STORE_HANDLE }.myshopify.com/admin/api/2024-10/graphql.json`,
-    headers: {
-      'X-Shopify-Access-Token': API_KEY,
-    },
-  });
-
   const response = await shopifyFetchClient.fetch({
-    method: 'POST',
+    method: 'post',
     body: {
       query: `query { order(id: "${ orderGid }") { id name createdAt displayFinancialStatus displayFulfillmentStatus } }`,
     },
-    responseInterpreter: shopifyResponseHandler,
+    context: {
+      creds,
+    },
   });
 
   if (!response.ok) {
