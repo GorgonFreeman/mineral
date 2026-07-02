26bb27f18981b8db33f340d2961091648d6100e2
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-02 11:05:09 +1000

    comment out premature result path

--- api/shopify/shopifyMutationDo.js
+++ api/shopify/shopifyMutationDo.js
@@ -48,7 +48,7 @@ const shopifyMutationDo = async (
     context: {
       creds,
       apiVersion,
-      resultsNode: mutationName,
+      // resultPath: `data.${ mutationName }`,
     },
     ...clientOptions,
   });
