955090973cd739cbb85dfdafbd6e4e7342a67bdd
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-03 10:55:09 +1000

    compose handle mutation errors in shopifyClient

--- api/shopify/shopify.utils.js
+++ api/shopify/shopify.utils.js
@@ -1,5 +1,58 @@
 const { DEFAULT_API_VERSION } = require('../shopify/shopify.constants');
-const { logDeep, pathAsArray, objectDigNodeAtPath, askQuestion, FetchClient, Chain, appendUrlToBase, fetchClientCommonSteps } = require('../utils');
+const { FetchClient, Chain, appendUrlToBase, fetchClientCommonSteps } = require('../utils');
+
+const handleMutationUserErrors = async (state) => {
+  const { response } = state;
+  const mutationPayload = response?.data;
+
+  if (!response?.ok || !mutationPayload || typeof mutationPayload !== 'object') {
+    return {};
+  }
+
+  const { userErrors, ...mutationResult } = mutationPayload;
+
+  if (!Array.isArray(userErrors)) {
+    return {};
+  }
+
+  const primaryData = Object.values(mutationResult).find((value) => value != null);
+
+  if (userErrors.length) {
+    const message = userErrors
+      .map((userError) => userError?.message)
+      .filter(Boolean)
+      .join('; ');
+
+    return {
+      response: {
+        ok: false,
+        error: {
+          code: 'USER_ERROR',
+          message: message || 'Mutation failed',
+          details: userErrors,
+        },
+        ...(primaryData !== undefined ? { data: primaryData } : {}),
+      },
+      breakChain: true,
+    };
+  }
+
+  const resultKeys = Object.keys(mutationResult).filter((key) => mutationResult[key] != null);
+
+  if (resultKeys.length === 1) {
+    return {
+      response: {
+        data: mutationResult[resultKeys[0]],
+      },
+    };
+  }
+
+  return {
+    response: {
+      data: mutationResult,
+    },
+  };
+};
 
 const addUrlAndAuthHeaders = async (state) => {
   const { requestPayload, context } = state;
@@ -38,6 +91,7 @@ const shopifyClientResponseInterpreter = new Chain([
   fetchClientCommonSteps.exitEarlyOnNotOk,
   fetchClientCommonSteps.exitEarlyOnGraphqlErrors,
   fetchClientCommonSteps.digToPath,
+  handleMutationUserErrors,
 ]);
 
 const shopifyClient = new FetchClient({
