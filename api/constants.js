113b94a122ea4b58de89243f1d83920d18b22d89
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-04 09:36:05 +1000

    HOSTED constant

---
+++ api/constants.js
@@ -0,0 +1,3 @@
+module.exports = {
+  HOSTED: !!process.env.HOSTED,
+};
--- api/utils.js
+++ api/utils.js
@@ -2,6 +2,7 @@ const xml2js = require('xml2js');
 const readline = require('readline');
 const fs = require('fs').promises;
 const yaml = require('yaml');
+const { HOSTED } = require('./constants');
 
 const wait = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms));
 
