44f9da37eca5eed249be6bed0f75d32b0b3ea155
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-04 00:42:13 +1000

    shopifyGiftCardCreate example commands

--- api/shopify/shopifyGiftCardCreate.js
+++ api/shopify/shopifyGiftCardCreate.js
@@ -68,6 +68,29 @@ curl -X POST "http://localhost:8000/shopifyGiftCardCreate" \
   -H "Content-Type: application/json" \
   -d '{
     "credsPayload": { "credsPath": "shopify.au" },
-    "thingId": "104188477512"
+    "giftCardInput": {
+      "initialValue": "5",
+      "customerId": "gid://shopify/Customer/1234567890",
+      "note": "Pocket money"
+    }
+  }'
+
+curl -X POST "http://localhost:8000/shopifyGiftCardCreate" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" },
+    "giftCardInput": {
+      "initialValue": "101.0",
+      "customerId": "gid://shopify/Customer/1234567890",
+      "recipientAttributes": {
+        "id": "gid://shopify/Customer/0987654321",
+        "message": "One dalmatian = one dollar",
+        "preferredName": "Cruella",
+        "sendNotificationAt": "2026-07-01T12:00:00Z"
+      }
+    },
+    "options": {
+      "returnGiftCardAttrs": "id initialValue { amount } customer { id } recipientAttributes { message preferredName sendNotificationAt recipient { id } }"
+    }
   }'
 */
