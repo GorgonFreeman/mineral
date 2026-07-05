db212e49b2f91abdd202a640bcca8cec99b0fd3e
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-06 00:18:26 +1000

    compose migrate yotpoCustomerPointsAdjust

---
+++ api/yotpo/yotpoCustomerPointsAdjust.js
@@ -0,0 +1,94 @@
+// https://loyaltyapi.yotpo.com/reference/adjust-a-customers-point-balance
+
+const { credsFromPayload, objHasAny, responseIfRejectingArgs } = require('../utils');
+const { credsValidator } = require('../validators');
+const { yotpoClient } = require('../yotpo/yotpo.utils');
+
+const customerIdentifierValidator = (customerIdentifier) => {
+  return objHasAny(customerIdentifier, [
+    'customerId', 
+    'customerEmail',
+  ]);
+};
+
+const pointsAmountValidator = Number;
+
+const validatorsByArg = {
+  credsPayload: credsValidator,
+  customerIdentifier: customerIdentifierValidator,
+  pointsAmount: pointsAmountValidator,
+};
+
+const yotpoCustomerPointsAdjust = async (
+  credsPayload,
+  customerIdentifier,
+  pointsAmount,
+  {
+    apiVersion,
+    applyAdjustmentToPointsEarned,
+    historyTitle,
+    visibleToCustomer = true,
+  } = {},
+) => {
+
+  const rejectResponse = await responseIfRejectingArgs(validatorsByArg, {
+    credsPayload,
+    customerIdentifier,
+    pointsAmount,
+  });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  const creds = await credsFromPayload(credsPayload);
+
+  const {
+    customerId,
+    customerEmail,
+  } = customerIdentifier;
+
+  const body = {
+    point_adjustment_amount: Number(pointsAmount),
+    ...(customerId && { customer_id: customerId }),
+    ...(customerEmail && { customer_email: customerEmail }),
+    ...(historyTitle && { history_title: historyTitle }),
+    ...(applyAdjustmentToPointsEarned !== undefined && { apply_adjustment_to_points_earned: applyAdjustmentToPointsEarned }),
+    visible_to_customer: visibleToCustomer,
+  };
+
+  const response = await yotpoClient.fetch({
+    url: '/points/adjust',
+    method: 'post',
+    body,
+    context: {
+      creds,
+      apiVersion,
+    },
+  });
+
+  return response;
+};
+
+const funcApiConfig = {
+  argNames: [
+    'credsPayload', 
+    'customerIdentifier', 
+    'pointsAmount',
+  ],
+  validatorsByArg,
+};
+
+module.exports = {
+  yotpoCustomerPointsAdjust,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/yotpoCustomerPointsAdjust" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "yotpo.au" },
+    "customerIdentifier": { "customerEmail": "john@whitefoxboutique.com" },
+    "pointsAmount": 100
+  }'
+*/
