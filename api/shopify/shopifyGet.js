375b611c079e9515b68153b7f60162e235813e19
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-06 15:35:10 +1000

    port over the rest of the logic and options from bedrock

--- api/shopify/shopifyGet.js
+++ api/shopify/shopifyGet.js
@@ -14,7 +14,27 @@ const shopifyGetPacket = async (
   resources,
   attrs,
   {
+    apiVersion,
+
+    perPage = 250,
     cursor,
+    queries,
+    reverse,
+    savedSearchId,
+    sortKey,
+    roles,
+    names,
+    constraintStatus,
+    constraintSubtype,
+    namespace,
+    ownerType,
+    pinnedStatus,
+    ownerGid,
+    includeClosed,
+    includeLegacy,
+    type,
+    sku,
+    argumentTypeOverrides = {},
   } = {},
 ) => {
 
@@ -22,12 +42,79 @@ const shopifyGetPacket = async (
   const Resource = capitaliseString(resource);
   const Resources = capitaliseString(resources);
 
+  const queryTypeDeclaration = [
+    '$first: Int!',
+    '$cursor: String',
+    ...queries ? ['$query: String,'] : [],
+    ...reverse !== undefined ? ['$reverse: Boolean,'] : [],
+    ...savedSearchId ? ['$savedSearchId: ID,'] : [],
+    ...sortKey ? [`$sortKey: ${ Resource }SortKeys,`] : [],
+    ...roles ? [`$roles: [${ Resource }Role!],`] : [],
+    ...names ? ['$names: [String!],'] : [],
+    ...constraintStatus ? ['$constraintStatus: MetafieldDefinitionConstraintStatus,'] : [],
+    ...constraintSubtype ? ['$constraintSubtype: MetafieldDefinitionConstraintSubtypeIdentifier,'] : [],
+    ...namespace ? ['$namespace: String,'] : [],
+    ...ownerType ? ['$ownerType: MetafieldOwnerType!,'] : [],
+    ...pinnedStatus ? ['$pinnedStatus: MetafieldDefinitionPinnedStatus,'] : [],
+    ...ownerGid ? ['$owner: ID!,'] : [],
+    ...includeClosed ? ['$includeClosed: Boolean,'] : [],
+    ...includeLegacy ? ['$includeLegacy: Boolean,'] : [],
+    ...type ? [`$type: ${ argumentTypeOverrides['type'] || 'String!' },`] : [],
+    ...sku ? ['$sku: String,'] : [],
+  ].join('\n');
+
+  const queryVariableDeclaration = [
+    'first: $first',
+    'after: $cursor',
+    ...queries ? ['query: $query'] : [],
+    ...reverse !== undefined ? ['reverse: $reverse'] : [],
+    ...savedSearchId ? ['savedSearchId: $savedSearchId'] : [],
+    ...sortKey ? ['sortKey: $sortKey'] : [],
+    ...roles ? ['roles: $roles'] : [],
+    ...names ? ['names: $names'] : [],
+    ...constraintStatus ? ['constraintStatus: $constraintStatus'] : [],
+    ...constraintSubtype ? ['constraintSubtype: $constraintSubtype'] : [],
+    ...namespace ? ['namespace: $namespace'] : [],
+    ...ownerType ? ['ownerType: $ownerType'] : [],
+    ...pinnedStatus ? ['pinnedStatus: $pinnedStatus'] : [],
+    ...ownerGid ? ['owner: $owner'] : [],
+    ...includeClosed ? ['includeClosed: $includeClosed'] : [],
+    ...includeLegacy ? ['includeLegacy: $includeLegacy'] : [],
+    ...type ? ['type: $type'] : [],
+    ...sku ? ['sku: $sku'] : [],
+  ].join('\n');
+
+  const variables = {
+    first: perPage,
+    cursor,
+    ...reverse !== undefined && { reverse },
+    ...queries && { query: queries.join(' AND ') },
+    ...savedSearchId && { savedSearchId: `gid://shopify/SavedSearch/${ savedSearchId }` },
+    ...sortKey && { sortKey },
+    ...roles && { roles },
+    ...names && { names },
+    ...constraintStatus && { constraintStatus },
+    ...constraintSubtype && { constraintSubtype },
+    ...namespace && { namespace },
+    ...ownerType && { ownerType },
+    ...pinnedStatus && { pinnedStatus },
+    ...ownerGid && { owner: ownerGid },
+    ...includeClosed && { includeClosed },
+    ...includeLegacy && { includeLegacy },
+    ...type && { type },
+    ...sku && { sku },
+  };
+
   return await shopifyClient.fetch({
     method: 'post',
     body: {
       query: `
-        query ${ Resources }Get($first: Int!, $after: String) {
-          ${ resources }(first: $first, after: $after) {
+        query Get${ Resources } (
+          ${ queryTypeDeclaration }
+        ) {
+          ${ resources }(
+            ${ queryVariableDeclaration }
+          ) {
             edges {
               node {
                 ${ attrs }
@@ -40,13 +127,11 @@ const shopifyGetPacket = async (
           }
         }
       `,
-      variables: {
-        first: 250,
-        ...(cursor ? { after: cursor } : {}),
-      },
+      variables,
     },
     context: {
       creds,
+      apiVersion,
       resultPath: `data.${ resources }`,
     },
   });
@@ -98,8 +183,40 @@ const shopifyGet = async (
   credsPayload,
   resource,
   {
-    resources = `${ resource }s`,
+    apiVersion,
+
+    // Related to the actual query
+    perPage = 250,
+    cursor,
     attrs = 'id',
+    queries,
+    reverse,
+    savedSearchId,
+    sortKey,
+    // https://shopify.dev/docs/api/admin-graphql/latest/queries/themes#arguments-names
+    roles,
+    names,
+    // https://shopify.dev/docs/api/admin-graphql/unstable/queries/metafielddefinitions
+    constraintStatus,
+    constraintSubtype,
+    namespace,
+    ownerType,
+    pinnedStatus,
+    // https://shopify.dev/docs/api/admin-graphql/unstable/queries/metafields#arguments-owner
+    ownerGid,
+    // https://shopify.dev/docs/api/admin-graphql/latest/objects/fulfillmentOrder#queries
+    includeClosed,
+    // https://shopify.dev/docs/api/admin-graphql/latest/queries/locations#arguments-includeLegacy
+    includeLegacy,
+    // https://shopify.dev/docs/api/admin-graphql/latest/queries/metaobjects
+    type,
+    // https://shopify.dev/docs/api/admin-graphql/latest/queries/inventoryItems
+    sku,
+    
+    // Helpers
+    resources = `${ resource }s`, // for when plural of the resource isn't `${ resource }s`
+    argumentTypeOverrides = {}, // for when a commonly-named API option is not just a single type, e.g. type being String vs CatalogType
+
     ...getterOptions // e.g. limit
   } = {},
 ) => {
@@ -120,8 +237,30 @@ const shopifyGet = async (
         creds, 
         resource, 
         resources, 
-        attrs
+        attrs,
       ],
+      options: {
+        apiVersion,
+        perPage,
+        cursor,
+        queries,
+        reverse,
+        savedSearchId,
+        sortKey,
+        roles,
+        names,
+        constraintStatus,
+        constraintSubtype,
+        namespace,
+        ownerType,
+        pinnedStatus,
+        ownerGid,
+        includeClosed,
+        includeLegacy,
+        type,
+        sku,
+        argumentTypeOverrides,
+      },
     },
     {
       func: shopifyGetPacket,
