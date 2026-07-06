c98ffccc3b5e76e96786fe4610a711d9f86e9383
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-06 17:26:59 +1000

    compose migrate shopifyTagsRemove

---
+++ api/shopify/shopifyTagsRemove.js
@@ -0,0 +1,99 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/mutations/tagsRemove
+
+const { credsValidator } = require('../validators');
+const { responseIfRejectingArgs, actionSingleOrMultiple } = require('../utils');
+const { shopifyMutationDo } = require('../shopify/shopifyMutationDo');
+
+const validatorsByArg = {
+  credsPayload: credsValidator,
+  gid: Boolean,
+  tags: Array,
+};
+
+const defaultAttrs = 'id';
+
+const shopifyTagsRemoveSingle = async (
+  credsPayload,
+  gid,
+  tags,
+  {
+    apiVersion,
+    returnAttrs = defaultAttrs,
+  } = {},
+) => {
+
+  return shopifyMutationDo(
+    credsPayload,
+    'tagsRemove',
+    {
+      mutationVariables: {
+        id: {
+          type: 'ID!',
+          value: gid,
+        },
+        tags: {
+          type: '[String!]!',
+          value: tags,
+        },
+      },
+      returnSchema: `node { ${ returnAttrs } }`,
+      apiVersion,
+    },
+  );
+};
+
+const shopifyTagsRemove = async (
+  credsPayload,
+  gid,
+  tags,
+  {
+    queueRunOptions,
+    apiVersion,
+    returnAttrs = defaultAttrs,
+  } = {},
+) => {
+
+  const rejectResponse = await responseIfRejectingArgs(validatorsByArg, {
+    credsPayload,
+    gid,
+    tags,
+  });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  return actionSingleOrMultiple(
+    gid,
+    shopifyTagsRemoveSingle,
+    (gidItem) => ({
+      args: [credsPayload, gidItem, tags, { apiVersion, returnAttrs }],
+    }),
+    {
+      ...(queueRunOptions ? { queueRunOptions } : {}),
+    },
+  );
+};
+
+const funcApiConfig = {
+  argNames: [
+    'credsPayload',
+    'gid',
+    'tags',
+  ],
+  validatorsByArg,
+};
+
+module.exports = {
+  shopifyTagsRemove,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/shopifyTagsRemove" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" },
+    "gid": "gid://shopify/Product/6981195825224",
+    "tags": ["watermelon", "banana"]
+  }'
+*/
