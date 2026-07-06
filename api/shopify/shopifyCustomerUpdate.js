503a5a1b60f2cccce96910671e2057967d7434c9
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-06 17:29:22 +1000

    shopifyCustomerUpdate complete function

---
+++ api/shopify/shopifyCustomerUpdate.js
@@ -0,0 +1,80 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/mutations/customerUpdate
+
+const { credsValidator } = require('../validators');
+const { responseIfRejectingArgs } = require('../utils');
+const { shopifyMutationDo } = require('../shopify/shopifyMutationDo');
+
+const validatorsByArg = {
+  credsPayload: credsValidator,
+  customerId: Boolean,
+  updatePayload: Boolean,
+};
+
+const defaultReturnCustomerAttrs = 'id email firstName lastName phone';
+
+const shopifyCustomerUpdate = async (
+  credsPayload,
+  customerId,
+  updatePayload,
+  {
+    apiVersion,
+    returnCustomerAttrs = defaultReturnCustomerAttrs,
+  } = {},
+) => {
+
+  const rejectResponse = await responseIfRejectingArgs(validatorsByArg, {
+    credsPayload,
+    customerId,
+    updatePayload,
+  });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  return shopifyMutationDo(
+    credsPayload,
+    'customerUpdate',
+    {
+      mutationVariables: {
+        input: {
+          type: 'CustomerInput!',
+          value: {
+            id: `gid://shopify/Customer/${ customerId }`,
+            ...updatePayload,
+          },
+        },
+      },
+      returnSchema: `
+        customer { ${ returnCustomerAttrs } }
+      `.trim(),
+      apiVersion,
+    },
+  );
+};
+
+const funcApiConfig = {
+  argNames: [
+    'credsPayload',
+    'customerId',
+    'updatePayload',
+  ],
+  validatorsByArg,
+};
+
+module.exports = {
+  shopifyCustomerUpdate,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/shopifyCustomerUpdate" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" },
+    "customerId": "8575963103304",
+    "updatePayload": {
+      "firstName": "Winston",
+      "lastName": "Churchill"
+    }
+  }'
+*/
