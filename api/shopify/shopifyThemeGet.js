2654937826e9fae3fa7635b532d7ecbef0a7ff06
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-08 14:32:12 +1000

    undo getting theme by name support

--- api/shopify/shopifyThemeGet.js
+++ api/shopify/shopifyThemeGet.js
@@ -3,84 +3,41 @@
 const { credsValidator } = require('../validators');
 const { ArgsWarden, objHasAny } = require('../utils');
 const { shopifyGetSingle } = require('../shopify/shopifyGetSingle');
-const { shopifyThemesGet } = require('../shopify/shopifyThemesGet');
 
 const defaultAttrs = 'id name role createdAt updatedAt';
 
-const themeIdentifierValidator = (themeIdentifier) => {
-  return objHasAny(themeIdentifier, [
-    'themeId',
-    'themeName',
-  ]);
-};
-
 const argsWarden = new ArgsWarden([
   ['credsPayload', credsValidator],
-  ['themeIdentifier', themeIdentifierValidator],
+  ['themeId'],
 ]);
 
 const shopifyThemeGet = async (
   credsPayload,
-  themeIdentifier,
+  themeId,
   {
     apiVersion,
     attrs = defaultAttrs,
   } = {},
 ) => {
 
-  const rejectResponse = await argsWarden.responseIfRejectingArgs({ credsPayload, themeIdentifier });
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({ 
+    credsPayload, 
+    themeId,
+  });
   if (rejectResponse) {
     return rejectResponse;
   }
 
-  const {
-    themeId,
-    themeName,
-  } = themeIdentifier;
-
-  if (themeId) {
-    return shopifyGetSingle(
-      credsPayload,
-      'theme',
-      themeId,
-      {
-        apiVersion,
-        attrs,
-        gidType: 'OnlineStoreTheme',
-      },
-    );
-  }
-
-  const themesResponse = await shopifyThemesGet(
+  return shopifyGetSingle(
     credsPayload,
+    'theme',
+    themeId,
     {
       apiVersion,
       attrs,
-      names: [themeName],
-      limit: 1,
+      gidType: 'OnlineStoreTheme',
     },
   );
-
-  if (!themesResponse.ok) {
-    return themesResponse;
-  }
-
-  const themes = themesResponse.data;
-
-  if (!Array.isArray(themes) || !themes.length) {
-    return {
-      ok: false,
-      error: {
-        code: 'NOT_FOUND',
-        message: `No theme found with name "${ themeName }"`,
-      },
-    };
-  }
-
-  return {
-    ok: true,
-    data: themes[0],
-  };
 };
 
 const funcApiConfig = {
@@ -97,6 +54,6 @@ curl -X POST "http://localhost:8000/shopifyThemeGet" \
   -H "Content-Type: application/json" \
   -d '{
     "credsPayload": { "credsPath": "shopify.au" },
-    "themeIdentifier": { "themeId": "142724202556" }
+    "themeId": "129840808008"
   }'
 */
