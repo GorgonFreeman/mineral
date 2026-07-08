8d556624396e645ce45f6f6c560346dc8ee3e606
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-08 12:57:06 +1000

    shopifyCollectionGet stub

---
+++ api/shopify/shopifyCollectionGet.js
@@ -0,0 +1,55 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/queries/page
+
+const { credsValidator } = require('../validators');
+const { ArgsWarden } = require('../utils');
+const { shopifyGetSingle } = require('../shopify/shopifyGetSingle');
+
+const defaultAttrs = 'id title handle';
+
+const argsWarden = new ArgsWarden([
+  ['credsPayload', credsValidator],
+  ['thingId'],
+]);
+
+const shopifyCollectionGet = async (
+  credsPayload,
+  thingId,
+  {
+    apiVersion,
+    attrs = defaultAttrs,
+  } = {},
+) => {
+
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({ credsPayload, thingId });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  return shopifyGetSingle(
+    credsPayload,
+    'thing',
+    thingId,
+    {
+      apiVersion,
+      attrs,
+    },
+  );
+};
+
+const funcApiConfig = {
+  argsWarden,
+};
+
+module.exports = {
+  shopifyCollectionGet,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/shopifyCollectionGet" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" },
+    "thingId": "104188477512"
+  }'
+*/
