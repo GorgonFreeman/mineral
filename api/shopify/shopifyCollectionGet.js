d2d75b659142b08283c28632a22608498fdf9fb4
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-08 13:07:18 +1000

    broaden shopifyCollectionGet to get by identifier

--- api/shopify/shopifyCollectionGet.js
+++ api/shopify/shopifyCollectionGet.js
@@ -1,34 +1,81 @@
 // https://shopify.dev/docs/api/admin-graphql/latest/queries/collection
 
 const { credsValidator } = require('../validators');
-const { ArgsWarden } = require('../utils');
+const { ArgsWarden, objHasAny, credsFromPayload } = require('../utils');
 const { shopifyGetSingle } = require('../shopify/shopifyGetSingle');
+const { shopifyClient } = require('../shopify/shopify.utils');
 
 const defaultAttrs = 'id title handle';
 
+const collectionIdentifierValidator = (collectionIdentifier) => {
+  return objHasAny(collectionIdentifier, [
+    'customId',
+    'id',
+    'handle',
+  ]);
+};
+
 const argsWarden = new ArgsWarden([
   ['credsPayload', credsValidator],
-  ['collectionId'],
+  ['collectionIdentifier', collectionIdentifierValidator],
 ]);
 
 const shopifyCollectionGet = async (
   credsPayload,
-  collectionId,
+  collectionIdentifier,
   {
     apiVersion,
     attrs = defaultAttrs,
   } = {},
 ) => {
 
-  const rejectResponse = await argsWarden.responseIfRejectingArgs({ credsPayload, collectionId });
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({ credsPayload, collectionIdentifier });
   if (rejectResponse) {
     return rejectResponse;
+  }
+
+  const { 
+    customId, 
+    id, 
+    handle,
+  } = collectionIdentifier;
+
+  if (!id) {
+
+    const creds = await credsFromPayload(credsPayload);
+
+    const query = `
+      query GetCollectionByIdentifier ($identifier: CollectionIdentifierInput!) {
+        collection: collectionByIdentifier(identifier: $identifier) {
+          ${ attrs }
+        }
+      }
+    `;
+
+    const variables = {
+      identifier: {
+        ...customId && { customId },
+        ...handle && { handle },
+      },
+    };
+
+    const response = await shopifyClient.fetch({
+      method: 'post',
+      body: { query, variables },
+      context: {
+        creds,
+        apiVersion,
+        resultPath: 'data.collection',
+      },
+    });
+
+    return response;
   }
 
   return shopifyGetSingle(
     credsPayload,
     'collection',
-    collectionId,
+    collectionIdentifier,
     {
       apiVersion,
       attrs,
@@ -50,6 +97,13 @@ curl -X POST "http://localhost:8000/shopifyCollectionGet" \
   -H "Content-Type: application/json" \
   -d '{
     "credsPayload": { "credsPath": "shopify.au" },
-    "collectionId": "279980277832"
+    "collectionIdentifier": { "id": "279980277832" }
+  }'
+
+curl -X POST "http://localhost:8000/shopifyCollectionGet" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" },
+    "collectionIdentifier": { "handle": "new-arrivals" }
   }'
 */
