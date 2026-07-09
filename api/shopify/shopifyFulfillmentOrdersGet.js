c4091cde80fefcb076a84781eabc8ca293fa4bda
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-09 19:20:52 +1000

    shopifyFulfillmentOrdersGet complete function

---
+++ api/shopify/shopifyFulfillmentOrdersGet.js
@@ -0,0 +1,46 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/queries/fulfillment-orders
+
+const { ArgsWarden } = require('../utils');
+const { credsValidator } = require('../validators');
+const { shopifyGet } = require('./shopifyGet');
+
+const argsWarden = new ArgsWarden([
+  ['credsPayload', credsValidator],
+]);
+
+const shopifyFulfillmentOrdersGet = async (
+  credsPayload,
+  {
+    ...getterOptions // e.g. limit
+  } = {},
+) => {
+
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({ 
+    credsPayload,
+  });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  return await shopifyGet(credsPayload, 'fulfillmentOrder', { ...getterOptions });
+};
+
+const funcApiConfig = {
+  argsWarden,
+};
+
+module.exports = {
+  shopifyFulfillmentOrdersGet,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/shopifyFulfillmentOrdersGet" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" },
+    "options": {
+      "limit": 10
+    }
+  }'
+*/
