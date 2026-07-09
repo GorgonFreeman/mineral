7a9032835660dc458b8e41769bf841525e759b3b
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-09 21:16:41 +1000

    shopifyEventsGet initial function
    not working because of internal server error

---
+++ api/shopify/shopifyEventsGet.js
@@ -0,0 +1,46 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/queries/events
+
+const { ArgsWarden } = require('../utils');
+const { credsValidator } = require('../validators');
+const { shopifyGet } = require('./shopifyGet');
+
+const argsWarden = new ArgsWarden([
+  ['credsPayload', credsValidator],
+]);
+
+const shopifyEventsGet = async (
+  credsPayload,
+  {
+    ...getterOptions // e.g. limit
+  } = {},
+) => {
+
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({ 
+    credsPayload,
+  });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  return await shopifyGet(credsPayload, 'event', { ...getterOptions });
+};
+
+const funcApiConfig = {
+  argsWarden,
+};
+
+module.exports = {
+  shopifyEventsGet,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/shopifyEventsGet" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" },
+    "options": {
+      "limit": 10
+    }
+  }'
+*/
