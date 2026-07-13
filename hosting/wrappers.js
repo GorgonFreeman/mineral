f5589374197df3f1f059914012f9e70c7b98fc30
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-13 16:24:34 +1000

    allow x-wf-app through

--- hosting/wrappers.js
+++ hosting/wrappers.js
@@ -22,7 +22,7 @@ const allowCrossOriginCallsAndHandleOptions = async (req, res) => {
 
   res.setHeader('Access-Control-Allow-Origin', origin || '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-api-key, x-wf-token, x-wf-value');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-api-key, x-wf-token, x-wf-value, x-wf-app');
 
   if (req.method === 'OPTIONS') {
     res.writeHead(204);
