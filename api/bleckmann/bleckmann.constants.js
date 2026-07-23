8a1f520837d8cebe06f72e202ab1d0772311f63d
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-23 14:31:07 +1000

    wrong base url

--- api/bleckmann/bleckmann.constants.js
+++ api/bleckmann/bleckmann.constants.js
@@ -1,5 +1,5 @@
 // TODO: Split out API version
-const BASE_URL = 'https://apis.bleckmann.com/warehousing/v1.6.0';
+const BASE_URL = 'https://apis.bleckmann.com';
 const MAX_PER_PAGE = 1000; // The 'limit' query parameter must be an integer from 1 to 1000
 
 module.exports = {
