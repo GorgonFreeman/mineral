ce068a282bf95187b6bcb1a08a68c423eb70e364
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-23 14:13:50 +1000

    useBaseUrl pipeline initial

---
+++ api/bleckmann/bleckmann.constants.js
@@ -0,0 +1,6 @@
+// TODO: Split out API version
+const BASE_URL = 'https://apis.bleckmann.com/warehousing/v1.6.0';
+
+module.exports = {
+  BASE_URL,
+};
--- api/bleckmann/bleckmannAsnGet.js
+++ api/bleckmann/bleckmannAsnGet.js
@@ -1,5 +1,6 @@
 const { credsValidator } = require('../validators');
-const { ArgsWarden, FetchClientV2 } = require('../utils');
+const { ArgsWarden, FetchClientV2, useBaseUrl, appendUrlToBase, logDeep } = require('../utils');
+const { BASE_URL } = require('../bleckmann/bleckmann.constants');
 
 const argsWarden = new ArgsWarden([
   ['credsPayload', credsValidator],
@@ -20,13 +21,32 @@ const bleckmannAsnGet = async (
     return rejectResponse;
   }
 
-  const bleckmannFetchClient = new FetchClientV2();
+  const useBaseUrl = (state) => {
+    const { context, requestPayload } = state;
+    const { baseUrl } = context;
+    const { url } = requestPayload;
+    logDeep(state);
+    return {
+      requestPayload: {
+        ...requestPayload,
+        url: appendUrlToBase(baseUrl, url),
+      },
+    };
+  };
+
+  const bleckmannFetchClient = new FetchClientV2({
+    pipeline: [
+      useBaseUrl,
+      'fetch',
+    ],
+  });
   const response = await bleckmannFetchClient.fetch({
     requestPayload: {
       url: `/warehousing/asns/${ asnId }`,
     },
     context: {
       credsPayload,
+      baseUrl: BASE_URL,
     },
   });
 
