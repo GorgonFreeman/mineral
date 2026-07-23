719c89df2984d91bfa7541f1f0f6f22d5c1800a5
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-23 14:19:14 +1000

    compose bleckmannAsnsGet initial

--- api/bleckmann/bleckmann.constants.js
+++ api/bleckmann/bleckmann.constants.js
@@ -1,6 +1,8 @@
 // TODO: Split out API version
 const BASE_URL = 'https://apis.bleckmann.com/warehousing/v1.6.0';
+const MAX_PER_PAGE = 1000; // The 'limit' query parameter must be an integer from 1 to 1000
 
 module.exports = {
   BASE_URL,
+  MAX_PER_PAGE,
 };
---
+++ api/bleckmann/bleckmannAsnsGet.js
@@ -0,0 +1,66 @@
+// https://app.swaggerhub.com/apis-docs/Bleckmann/warehousing/1.5.2#/ASN/getAsns
+
+const { credsValidator } = require('../validators');
+const { ArgsWarden } = require('../utils');
+const { BASE_URL, MAX_PER_PAGE } = require('../bleckmann/bleckmann.constants');
+const { bleckmannClient } = require('../bleckmann/bleckmann.utils');
+
+const argsWarden = new ArgsWarden([
+  ['credsPayload', credsValidator],
+]);
+
+const bleckmannAsnsGet = async (
+  credsPayload,
+  {
+    createdFrom,
+    createdTo,
+    receivedFrom,
+    receivedTo,
+    status,
+    perPage = MAX_PER_PAGE,
+  } = {},
+) => {
+
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({ credsPayload });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  const response = await bleckmannClient.fetch({
+    requestPayload: {
+      url: '/warehousing/asns',
+      params: {
+        limit: perPage,
+        ...(createdFrom && { createdFrom }),
+        ...(createdTo && { createdTo }),
+        ...(receivedFrom && { receivedFrom }),
+        ...(receivedTo && { receivedTo }),
+        ...(status && { status }),
+      },
+    },
+    context: {
+      credsPayload,
+      baseUrl: BASE_URL,
+    },
+  });
+
+  return response;
+};
+
+const funcApiConfig = {
+  argsWarden,
+};
+
+module.exports = {
+  bleckmannAsnsGet,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/bleckmannAsnsGet" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "bleckmann" },
+    "options": { "perPage": 20 }
+  }'
+*/
