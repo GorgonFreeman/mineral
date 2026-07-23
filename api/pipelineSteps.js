8ef5cf76af1af537f06aac1ea20c4a34c18bde75
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-23 14:52:16 +1000

    return failure if no base url

--- api/pipelineSteps.js
+++ api/pipelineSteps.js
@@ -4,6 +4,19 @@ const useBaseUrl = (baseUrl) => async (state) => {
   const { requestPayload, context } = state;
   const resolvedBaseUrl = baseUrl ?? context.baseUrl;
 
+  if (!resolvedBaseUrl) {
+    return {
+      breakChain: true,
+      response: {
+        ok: false,
+        error: {
+          code: 'NO_BASE_URL',
+          message: 'No base URL provided',
+        },
+      },
+    };
+  }
+
   return {
     requestPayload: {
       ...requestPayload,
