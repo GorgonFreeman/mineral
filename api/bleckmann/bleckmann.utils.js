069417ca274a1d7514b8861a945d10d6a3df0310
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-23 14:33:37 +1000

    resolve creds and add auth headers pipeline steps

--- api/bleckmann/bleckmann.utils.js
+++ api/bleckmann/bleckmann.utils.js
@@ -1,10 +1,45 @@
-const { FetchClientV2, appendUrlToBase, logDeep } = require('../utils');
+const {
+  FetchClientV2,
+  appendUrlToBase,
+  credsFromPayload,
+} = require('../utils');
+
+const resolveCreds = async (state) => {
+  const { context } = state;
+
+  if (context.creds || !context.credsPayload) {
+    return {};
+  }
+
+  const creds = await credsFromPayload(context.credsPayload);
+
+  return {
+    context: {
+      creds,
+    },
+  };
+};
+
+const useAuthHeaders = async (state) => {
+  const { requestPayload, context } = state;
+  const { PRIMARY_KEY } = context.creds;
+
+  return {
+    requestPayload: {
+      ...requestPayload,
+      headers: {
+        'x-api-key': PRIMARY_KEY,
+        ...requestPayload.headers,
+      },
+    },
+  };
+};
 
 const useBaseUrl = (state) => {
   const { context, requestPayload } = state;
   const { baseUrl } = context;
   const { url } = requestPayload;
-  logDeep(state);
+
   return {
     requestPayload: {
       ...requestPayload,
@@ -15,6 +50,8 @@ const useBaseUrl = (state) => {
 
 const bleckmannClient = new FetchClientV2({
   pipeline: [
+    resolveCreds,
+    useAuthHeaders,
     useBaseUrl,
     'fetch',
   ],
