ffffbdf8785c5ef541b69a23c40ab83d0bb992c2
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-27 16:20:04 +1000

    shopifyStagedUploadCreate stub

---
+++ api/shopify/shopifyStagedUploadCreate.js
@@ -0,0 +1,58 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/mutations/stagedUploadsCreate
+
+const { credsValidator } = require('../validators');
+const { ArgsWarden } = require('../utils');
+const { shopifyMutationDo } = require('../shopify/shopifyMutationDo');
+
+const argsWarden = new ArgsWarden([
+  ['credsPayload', credsValidator],
+  ['thingId'],
+]);
+
+const shopifyStagedUploadCreate = async (
+  credsPayload,
+  thingId,
+  {
+    apiVersion,
+    returnSchema = 'deletedThingId',
+  } = {},
+) => {
+
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({ credsPayload, thingId });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  return shopifyMutationDo(
+    credsPayload,
+    'thingDelete',
+    {
+      mutationVariables: {
+        id: {
+          type: 'ID!',
+          value: `gid://shopify/Thing/${ thingId }`,
+        },
+      },
+      returnSchema,
+      apiVersion,
+    },
+  );
+};
+
+const funcApiConfig = {
+  argsWarden,
+};
+
+module.exports = {
+  shopifyStagedUploadCreate,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/shopifyStagedUploadCreate" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" },
+    "thingId": "104188477512"
+  }'
+*/
