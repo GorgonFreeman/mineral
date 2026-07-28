67b0c90e661b3ca9c653706926571f394a478cba
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-07-28 14:14:23 +1000

    compose and tweak shopifyTagsAddBulk

---
+++ api/shopify/shopifyTagsAddBulk.js
@@ -0,0 +1,77 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/mutations/tagsAdd
+
+const { credsValidator } = require('../validators');
+const { ArgsWarden } = require('../utils');
+const { shopifyBulkMutationDo } = require('./shopifyBulkMutationDo');
+
+const tagsAddBulkMutation = `
+  mutation call($id: ID!, $tags: [String!]!) {
+    tagsAdd(id: $id, tags: $tags) {
+      node {
+        id
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`;
+
+const argsWarden = new ArgsWarden([
+  ['credsPayload', credsValidator],
+  ['gids', Array.isArray],
+  ['tags', Array.isArray],
+]);
+
+const shopifyTagsAddBulk = async (
+  credsPayload,
+  gids,
+  tags,
+  {
+    ...bulkMutationOptions,
+  } = {},
+) => {
+
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({
+    credsPayload,
+    gids,
+    tags,
+  });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  return shopifyBulkMutationDo(
+    credsPayload,
+    {
+      mutation: tagsAddBulkMutation,
+      input: {
+        data: gids.map((gid) => ({
+          id: gid,
+          tags,
+        })),
+      },
+    },
+    bulkMutationOptions,
+  );
+};
+
+const funcApiConfig = {
+  argsWarden,
+};
+
+module.exports = {
+  shopifyTagsAddBulk,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/shopifyTagsAddBulk" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" },
+    "gids": ["gid://shopify/Customer/123", "gid://shopify/Customer/456"],
+    "tags": ["martian_sympathisers"]
+  }'
+*/
