bfe6b65466cb28a111caf7cdebde22774e90700e
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-08-01 11:39:51 +1000

    implement actionSingleOrMultiple in etsyListingInventoryUpdate

--- api/etsy/etsyListingInventoryUpdate.js
+++ api/etsy/etsyListingInventoryUpdate.js
@@ -1,19 +1,22 @@
 // https://developers.etsy.com/documentation/reference/#operation/updateListingInventory
 
-const { ArgsWarden } = require('../utils');
+const { ArgsWarden, actionSingleOrMultiple, everyIfArray, valueProvided } = require('../utils');
 const { credsValidator } = require('../validators');
 const { etsyClient } = require('./etsy.utils');
 
+const updatePayloadWithListingIdValidator = (updatePayloadWithListingId) => {
+  return valueProvided(updatePayloadWithListingId?.listingId)
+    && valueProvided(updatePayloadWithListingId?.updatePayload);
+};
+
 const argsWarden = new ArgsWarden([
   ['credsPayload', credsValidator],
-  ['listingId'],
-  ['updatePayload'],
+  ['updatePayloadWithListingId', (updatePayloadWithListingId) => everyIfArray(updatePayloadWithListingIdValidator, updatePayloadWithListingId)],
 ]);
 
-const etsyListingInventoryUpdate = async (
+const etsyListingupdatePayloadWithListingIdSingle = async (
   credsPayload,
-  listingId,
-  updatePayload,
+  updatePayloadWithListingId,
   {
     params,
     inspect = false,
@@ -21,20 +24,17 @@ const etsyListingInventoryUpdate = async (
   } = {},
 ) => {
 
-  const rejectResponse = await argsWarden.responseIfRejectingArgs({
-    credsPayload,
-    listingId,
-    updatePayload,
-  });
-  if (rejectResponse) {
-    return rejectResponse;
-  }
+  const { 
+    listingId, 
+    ...updatePayload 
+  } = updatePayloadWithListingId;
 
   return fetchClient.fetch({
     requestPayload: {
       method: 'put',
       url: `/application/listings/${ listingId }/inventory`,
       body: updatePayload,
+      ...(params && { params }),
     },
     context: {
       credsPayload,
@@ -44,23 +44,56 @@ const etsyListingInventoryUpdate = async (
   });
 };
 
+const etsyListingupdatePayloadWithListingId = async (
+  credsPayload,
+  updatePayloadWithListingId,
+  {
+    params,
+    inspect = false,
+    fetchClient = etsyClient,
+    queueRunOptions,
+  } = {},
+) => {
+
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({
+    credsPayload,
+    updatePayloadWithListingId,
+  });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  return actionSingleOrMultiple(
+    updatePayloadWithListingId,
+    etsyListingupdatePayloadWithListingIdSingle,
+    (updatePayloadWithListingIdItem) => ({
+      args: [credsPayload, updatePayloadWithListingIdItem, { params, inspect, fetchClient }],
+    }),
+    {
+      ...(queueRunOptions ? { queueRunOptions } : {}),
+    },
+  );
+};
+
 const funcApiConfig = {
   argsWarden,
 };
 
 module.exports = {
-  etsyListingInventoryUpdate,
+  etsyListingupdatePayloadWithListingId,
   funcApiConfig,
 };
 
 /*
-curl -X POST "http://localhost:8000/etsyListingInventoryUpdate" \
+curl -X POST "http://localhost:8000/etsyListingupdatePayloadWithListingId" \
   -H "Content-Type: application/json" \
   -d '{
     "credsPayload": {
       "credsPath": "etsy"
     },
-    "listingId": "1234567890",
-    "updatePayload": {}
+    "updatePayloadWithListingId": {
+      "listingId": "1234567890",
+      "updatePayload": {}
+    }
   }'
 */
