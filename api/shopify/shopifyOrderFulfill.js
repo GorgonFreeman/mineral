b4cde5e3c5bd803d41fcf4d2a901b256df69a156
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-08-07 14:02:05 +1000

    shopifyOrderFulfill stub and args setup

---
+++ api/shopify/shopifyOrderFulfill.js
@@ -0,0 +1,67 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/mutations/pageDelete
+
+const { credsValidator } = require('../validators');
+const { ArgsWarden } = require('../utils');
+const { shopifyMutationDo } = require('../shopify/shopifyMutationDo');
+
+const orderIdentifierValidator = (orderIdentifier) => {
+  return objHasAny(orderIdentifier, [
+    'orderId', 
+    'orderName',
+  ]);
+};
+
+const fulfillmentPayloadValidator = (fulfillmentPayload) => {
+  // TODO: Support fulfilling whole order, items by sku, or items by id
+  return true;
+};
+
+const argsWarden = new ArgsWarden([
+  ['credsPayload', credsValidator],
+  ['orderIdentifier', orderIdentifierValidator],
+  ['fulfillmentPayload', fulfillmentPayloadValidator],
+]);
+
+const shopifyOrderFulfill = async (
+  credsPayload,
+  orderIdentifier,
+  fulfillmentPayload,
+  {
+    apiVersion,
+    // TODO: Support return schema
+    // returnSchema = 'deletedThingId',
+  } = {},
+) => {
+
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({
+    credsPayload,
+    orderIdentifier,
+    fulfillmentPayload,
+  });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+  
+  return {
+    ok: true,
+  };
+};
+
+const funcApiConfig = {
+  argsWarden,
+};
+
+module.exports = {
+  shopifyOrderFulfill,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/shopifyOrderFulfill" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" },
+    "orderIdentifier": { "orderId": "104188477512" },
+    "fulfillmentPayload": { "all": true }
+  }'
+*/
