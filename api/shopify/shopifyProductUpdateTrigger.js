993f0e76c099adb72e92eea7b4a151cb745c0da8
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-09-04 10:35:12 +1000

    shopifyProductUpdateTrigger stub

---
+++ api/shopify/shopifyProductUpdateTrigger.js
@@ -0,0 +1,61 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/mutations/pageDelete
+
+const { credsValidator } = require('../validators');
+const { ArgsWarden } = require('../utils');
+const { shopifyMutationDo } = require('../shopify/shopifyMutationDo');
+
+const argsWarden = new ArgsWarden([
+  ['credsPayload', credsValidator],
+  ['thingId'],
+]);
+
+const shopifyProductUpdateTrigger = async (
+  credsPayload,
+  thingId,
+  {
+    apiVersion,
+    returnSchema = 'deletedThingId',
+  } = {},
+) => {
+
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({
+    credsPayload,
+    thingId,
+  });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  return shopifyMutationDo(
+    credsPayload,
+    'thingDelete',
+    {
+      mutationVariables: {
+        id: {
+          type: 'ID!',
+          value: `gid://shopify/Thing/${ thingId }`,
+        },
+      },
+      returnSchema,
+      apiVersion,
+    },
+  );
+};
+
+const funcApiConfig = {
+  argsWarden,
+};
+
+module.exports = {
+  shopifyProductUpdateTrigger,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/shopifyProductUpdateTrigger" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" },
+    "thingId": "104188477512"
+  }'
+*/
