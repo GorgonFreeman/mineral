51aaf79acfb577b87218c302491dc72d2dc6afa1
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-09-07 16:15:28 +1000

    compose using oneFromManyInResponse in bleckmannPickticketGet

--- api/bleckmann/bleckmannPickticketGet.js
+++ api/bleckmann/bleckmannPickticketGet.js
@@ -1,7 +1,7 @@
 // https://app.swaggerhub.com/apis-docs/Bleckmann/warehousing/1.5.2#/PICKTICKET/getPickticketForId
 
 const { credsValidator } = require('../validators');
-const { ArgsWarden, objHasAny } = require('../utils');
+const { ArgsWarden, objHasAny, oneFromManyInResponse } = require('../utils');
 const { bleckmannClient } = require('../bleckmann/bleckmann.utils');
 const { bleckmannPickticketsGet } = require('../bleckmann/bleckmannPickticketsGet');
 
@@ -54,36 +54,7 @@ const bleckmannPickticketGet = async (
     },
   );
 
-  if (!response.ok) {
-    return response;
-  }
-
-  const picktickets = Array.isArray(response.data)
-    ? response.data
-    : response.data?.items || response.data?.picktickets || [];
-
-  if (picktickets.length === 0) {
-    return {
-      ok: true,
-      data: null,
-    };
-  }
-
-  if (picktickets.length > 1) {
-    return {
-      ok: false,
-      error: {
-        code: 'MULTIPLE_RESULTS',
-        message: 'Multiple picktickets found',
-        details: picktickets,
-      },
-    };
-  }
-
-  return {
-    ok: true,
-    data: picktickets[0],
-  };
+  return oneFromManyInResponse(response, 'reference', pickticketReference);
 };
 
 const funcApiConfig = {
