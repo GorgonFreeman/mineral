cd70dccc120fbf43001f5c6c85d93ae8482b1019
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-09-14 11:53:47 +1000

    compose migrate shopifyInventoryQuantitiesSet

--- api/shopify/shopifyInventoryQuantitiesSet.js
+++ api/shopify/shopifyInventoryQuantitiesSet.js
@@ -1,8 +1,19 @@
-// https://shopify.dev/docs/api/admin-graphql/latest/mutations/pageDelete
+// https://shopify.dev/docs/api/admin-graphql/latest/mutations/inventorySetQuantities
 
 const { credsValidator } = require('../validators');
-const { ArgsWarden, objHasAll } = require('../utils');
+const {
+  ArgsWarden,
+  arrayToChunks,
+  actionSingleOrMultiple,
+  objHasAll,
+  valueProvided,
+} = require('../utils');
 const { shopifyMutationDo } = require('../shopify/shopifyMutationDo');
+const {
+  MAX_INVENTORY_QUANTITIES_PER_SET,
+  INVENTORY_TYPES,
+  INVENTORY_REASONS,
+} = require('../shopify/shopify.constants');
 
 const inventoryQuantityInputValidator = (p) => {
   return objHasAll(p, ['inventoryItemId', 'locationId', 'quantity']);
@@ -10,11 +21,58 @@ const inventoryQuantityInputValidator = (p) => {
 
 const argsWarden = new ArgsWarden([
   ['credsPayload', credsValidator],
-  ['inventoryType', p => ['available', 'on_hand'].includes(p)],
-  ['quantities', p => p.every(inventoryQuantityInputValidator)],
-  ['reason'], // TODO: Validate against list of possible reasons
+  ['inventoryType', p => INVENTORY_TYPES.includes(p)],
+  ['quantities', p => Array.isArray(p) && p.length > 0 && p.every(inventoryQuantityInputValidator)],
+  ['reason', p => INVENTORY_REASONS.includes(p)],
   ['idempotencyKey'],
 ]);
+
+const shopifyInventoryQuantitiesSetChunk = async (
+  credsPayload,
+  inventoryType,
+  quantities,
+  reason,
+  idempotencyKey,
+  {
+    apiVersion,
+    inventoryAdjustmentGroupReturnAttrs = defaultInventoryAdjustmentGroupReturnAttrs,
+    referenceDocumentUri,
+  } = {},
+) => {
+
+  const quantitiesForInput = quantities.map(({
+    inventoryItemId,
+    locationId,
+    quantity,
+    changeFromQuantity = null,
+  }) => ({
+    inventoryItemId: `gid://shopify/InventoryItem/${ inventoryItemId }`,
+    locationId: `gid://shopify/Location/${ locationId }`,
+    quantity,
+    changeFromQuantity,
+  }));
+
+  return shopifyMutationDo(
+    credsPayload,
+    'inventorySetQuantities',
+    {
+      mutationVariables: {
+        input: {
+          type: 'InventorySetQuantitiesInput!',
+          value: {
+            name: inventoryType,
+            reason,
+            quantities: quantitiesForInput,
+            ...(valueProvided(referenceDocumentUri) && { referenceDocumentUri }),
+          },
+        },
+      },
+      returnSchema: `inventoryAdjustmentGroup { ${ inventoryAdjustmentGroupReturnAttrs } }`,
+      apiVersion,
+      idempotencyKey,
+    },
+  );
+};
 
 const shopifyInventoryQuantitiesSet = async (
   credsPayload,
@@ -23,6 +81,7 @@ const shopifyInventoryQuantitiesSet = async (
   reason,
   idempotencyKey,
   {
+    queueRunOptions,
     apiVersion,
     inventoryAdjustmentGroupReturnAttrs = 'createdAt reason changes { name delta }',
     referenceDocumentUri,
@@ -40,18 +99,27 @@ const shopifyInventoryQuantitiesSet = async (
     return rejectResponse;
   }
 
-  return shopifyMutationDo(
-    credsPayload,
-    'thingDelete',
-    {
-      mutationVariables: {
-        id: {
-          type: 'ID!',
-          value: `gid://shopify/Thing/${ thingId }`,
+  const chunks = arrayToChunks(quantities, MAX_INVENTORY_QUANTITIES_PER_SET);
+
+  return actionSingleOrMultiple(
+    chunks,
+    shopifyInventoryQuantitiesSetChunk,
+    (chunk) => ({
+      args: [
+        credsPayload,
+        inventoryType,
+        chunk,
+        reason,
+        chunks.length === 1 ? idempotencyKey : `${ idempotencyKey }:${ chunks.indexOf(chunk) }`,
+        {
+          apiVersion,
+          inventoryAdjustmentGroupReturnAttrs,
+          referenceDocumentUri,
         },
-      },
-      returnSchema,
-      apiVersion,
+      ],
+    }),
+    {
+      ...(queueRunOptions ? { queueRunOptions } : {}),
     },
   );
 };
@@ -69,7 +137,15 @@ module.exports = {
 curl -X POST "http://localhost:8000/shopifyInventoryQuantitiesSet" \
   -H "Content-Type: application/json" \
   -d '{
-    "credsPayload": { "credsPath": "shopify.au" },
-    ...
+    "credsPayload": { "credsPath": "shopify.wakanda" },
+    "inventoryType": "on_hand",
+    "quantities": [{
+      "inventoryItemId": "1234567890",
+      "locationId": "1234567890",
+      "changeFromQuantity": 10,
+      "quantity": 5
+    }],
+    "reason": "correction",
+    "idempotencyKey": "..."
   }'
 */
