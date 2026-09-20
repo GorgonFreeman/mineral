ad2f68b6959790a8d10c35c159e2a24b875487f0
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-09-21 09:19:11 +1000

    compose shopifyShopGet

---
+++ api/shopify/shopifyShopGet.js
@@ -0,0 +1,79 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/queries/shop
+
+const { credsValidator } = require('../validators');
+const { ArgsWarden } = require('../utils');
+const { shopifyClient } = require('./shopify.utils');
+
+const defaultAttrs = `
+id
+  name
+  myshopifyDomain
+  primaryDomain { host url }
+`.trim();
+
+const argsWarden = new ArgsWarden([
+  ['credsPayload', credsValidator],
+]);
+
+const shopifyShopGet = async (
+  credsPayload,
+  {
+    apiVersion,
+    attrs = defaultAttrs,
+    fetchClient = shopifyClient,
+  } = {},
+) => {
+
+  const rejectResponse = await argsWarden.responseIfRejectingArgs({
+    credsPayload,
+  });
+  if (rejectResponse) {
+    return rejectResponse;
+  }
+
+  return fetchClient.fetch({
+    requestPayload: {
+      method: 'post',
+      body: {
+        query: `
+          query Shop {
+            shop {
+              ${ attrs }
+            }
+          }
+        `,
+      },
+    },
+    context: {
+      credsPayload,
+      apiVersion,
+      resultPath: 'data.shop',
+    },
+  });
+};
+
+const funcApiConfig = {
+  argsWarden,
+};
+
+module.exports = {
+  shopifyShopGet,
+  funcApiConfig,
+};
+
+/*
+curl -X POST "http://localhost:8000/shopifyShopGet" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.au" }
+  }'
+
+curl -X POST "http://localhost:8000/shopifyShopGet" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "credsPayload": { "credsPath": "shopify.staging" },
+    "options": {
+      "attrs": "id name metafield(namespace: \\"custom\\", key: \\"stores_audit\\") { id namespace key type value }"
+    }
+  }'
+*/
